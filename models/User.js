557543b2aeff0749f8e5eedf1de068ca6b736557
Coskun Cakir <coskuncakir@Coskuns-MacBook-Pro.local>
2019-12-20 14:00:55 +0000

    user model added

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);

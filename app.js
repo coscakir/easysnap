087cdbcc30eed73252f2191b29d24e8fd71ee1fb
Coskun Cakir <coskuncakir@Coskuns-MacBook-Pro.local>
2019-12-20 13:52:34 +0000

    mongodb connection implemented

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-const express = require("express");
-require("dotenv").config();
-const { ApolloServer } = require("apollo-server-express");
-const { importSchema } = require("graphql-import");
-
-const resolvers = require("./graphql/resolvers");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { ApolloServer } = require('apollo-server-express');
+const { importSchema } = require('graphql-import');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
-  typeDefs: importSchema("./graphql/types/schema.graphql"),
+  typeDefs: importSchema('./graphql/types/schema.graphql'),
   resolvers
 });
 
-console.log(process.env.DB_HOST);
+// DB Connection
+mongoose
+  .connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('Connected to DB'))
+  .catch(e => console.log(e));
+
 const app = express();
-server.applyMiddleware({
-  app
+server.applyMiddleware({ app });
+app.listen({ port: 4001 }, () => {
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
 });
-app.listen(
-  {
-    port: 4000
-  },
-  () => {
-    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
-  }
-);

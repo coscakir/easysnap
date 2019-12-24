3f5148bdf2e47a1e8c0c680f464d77ab9b25d3bb
Coskun Cakir <coskuncakir@Coskuns-MacBook-Pro.local>
2019-12-24 11:41:05 +0000

    sign in mutation implemented

--- graphql/resolvers/mutations/user.mutation.js
+++ graphql/resolvers/mutations/user.mutation.js
@@ -1,12 +1,7 @@
+const bcrypt = require('bcrypt');
+
 module.exports = {
-  createUser: async (parent, {
-    data: {
-      userName,
-      password
-    }
-  }, {
-    User
-  }) => {
+  createUser: async (parent, { data: { userName, password } }, { User }) => {
     const user = await User.findOne({
       userName
     });
@@ -19,5 +14,20 @@ module.exports = {
       userName,
       password
     }).save();
+  },
+
+  signIn: async (parent, { data: { userName, password } }, { User }) => {
+    const user = await User.findOne({ userName });
+    if (!user) {
+      throw new Error('User does not exist');
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      throw new Error('Wrong password');
+    }
+
+    return { token: 'test token' };
   }
 };
--- graphql/resolvers/queries/User.js
+++ graphql/resolvers/queries/User.js
@@ -1,6 +1,10 @@
 const User = {
-  snaps: async (parent, args, { Snap }) => {
-    return await Snap.find({ userId: parent._id });
+  snaps: async (parent, args, {
+    Snap
+  }) => {
+    return await Snap.find({
+      userId: parent._id
+    });
   }
 };
 
